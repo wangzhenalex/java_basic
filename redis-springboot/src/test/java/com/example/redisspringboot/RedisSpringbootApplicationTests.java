848fdcc2d2aab9a21f44081c49e6529d50402067
zhen wang <wangzhen_1990@163.com>
2024-01-27 20:49:24 +0800

    redis-springboot

---
+++ redis-springboot/src/test/java/com/example/redisspringboot/RedisSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RedisSpringbootApplication.class)
+class RedisSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ redis-springboot/src/test/java/com/example/redisspringboot/SpringCacheTestor.java
@@ -0,0 +1,59 @@
+//package com.example.redisspringboot;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import javax.annotation.Resource;
+//
+//@SpringBootTest
+//public class SpringCacheTestor {
+//    @Resource
+//    private EmpService empService;
+//
+//    @Test
+//    public void testFindById(){
+//        Emp emp = empService.findById(1001);
+//        emp = empService.findById(1001);
+//        emp = empService.findById(1001);
+//        emp = empService.findById(1001);
+//        emp = empService.findById(1001);
+//        emp = empService.findById(1001);
+//        System.out.println(emp.getName());
+//        emp = empService.findById(1000);
+//        emp = empService.findById(1000);
+//        emp = empService.findById(1000);
+//        System.out.println(emp.getName());
+//    }
+//
+//    @Test
+//    public void testEmpRank() {
+//        List<Emp> list = empService.getEmpRank();
+//        list = empService.getEmpRank();
+//        for(Emp emp:list){
+//            System.out.println(emp);
+//        }
+//    }
+//    @Test
+//    public void testCreate(){
+//        empService.create(new Emp(1002 , "emp" + new Date().getTime() , new Date() , 1234f , "MARKET"));
+//        empService.create(new Emp(1002 , "emp" + new Date().getTime() , new Date() , 1234f , "MARKET"));
+//        empService.create(new Emp(1002 , "emp" + new Date().getTime() , new Date() , 1234f , "MARKET"));
+//        Emp emp = empService.findById(1002);
+//        System.out.println(emp);
+//    }
+//
+//    @Test
+//    public void testUpdate(){
+//        empService.update(new Emp(1002 , "u-emp" + new Date().getTime() , new Date() , 1234f , "MARKET"));
+//        empService.update(new Emp(1002 , "u-emp" + new Date().getTime() , new Date() , 1234f , "MARKET"));
+//        empService.update(new Emp(1002 , "u-emp" + new Date().getTime() , new Date() , 1234f , "MARKET"));
+//        Emp emp = empService.findById(1002);
+//        System.out.println(emp);
+//    }
+//    @Test
+//    public void testDelete(){
+//        empService.delete(1002);
+//        //Emp emp = empService.findById(1002);
+//        //System.out.println(emp);
+//    }
+//}
