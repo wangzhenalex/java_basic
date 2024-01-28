abe35befb44fa028630aacff28dfc22805498978
zhen wang <wangzhen_1990@163.com>
2024-01-28 17:47:37 +0800

    声明式缓存

--- redis-springboot/src/main/java/com/example/redisspringboot/RedisSpringbootApplication.java
+++ redis-springboot/src/main/java/com/example/redisspringboot/RedisSpringbootApplication.java
@@ -2,8 +2,10 @@ package com.example.redisspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RedisSpringbootApplication {
 
     public static void main(String[] args) {
--- redis-springboot/src/main/java/com/example/redisspringboot/config/RedisTemplateConfiguration.java
+++ redis-springboot/src/main/java/com/example/redisspringboot/config/RedisTemplateConfiguration.java
@@ -2,13 +2,20 @@ package com.example.redisspringboot.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisTemplateConfiguration {
@@ -31,5 +38,34 @@ public class RedisTemplateConfiguration {
         //afterPropertiesSet和init-method之间的执行顺序是afterPropertiesSet 先执行，init-method 后执行。
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager1m(LettuceConnectionFactory factory){
+        //加载默认Spring Cache配置信息
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
+        //设置有效期为1小时
+        config = config.entryTtl(Duration.ofMinutes(1));
+        //说明缓存Key使用单冒号进行分割
+        config = config.computePrefixWith(cacheName -> cacheName + ":");
+        //Redis Key采用String直接存储
+        config = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+        //Redis Value则将对象采用JSON形式存储
+        config = config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        //不缓存Null值对象
+        config = config.disableCachingNullValues();
+
+        //实例化CacheManger缓存管理器
+        RedisCacheManager cacheManager = RedisCacheManager.RedisCacheManagerBuilder
+                //绑定REDIS连接工厂
+                .fromConnectionFactory(factory)
+                //绑定配置对象
+                .cacheDefaults(config)
+                //与声明式事务注解@Transactional进行兼容
+                .transactionAware()
+                //完成对象构建
+                .build();
+        return cacheManager;
+
     }
 }
